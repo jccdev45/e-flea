0b45ca85d2dd06a1336418bea93fcd1ed66d6b46
Jordan Cruz-Correa <jordancruz-correa@jordans-mbp.lan>
2019-12-08 17:06:56 -0500

    Created item/user model+migration

---
+++ models/item.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Item = sequelize.define(
+    "Item",
+    {
+      name: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      category: DataTypes.STRING,
+      photos: DataTypes.STRING,
+      location: DataTypes.STRING,
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "User",
+          key: "id",
+          as: "userId"
+        }
+      }
+    },
+    {}
+  );
+  Item.associate = function(models) {
+    // associations can be defined here
+    Item.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE"
+    });
+  };
+  return Item;
+};
