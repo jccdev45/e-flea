2d7135117675c9383ce7b7798f95707739675677
Yev <yevyork@gmail.com>
2019-12-09 14:05:11 -0500

    update routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+const restrict = require('../helpers')
+
+router.get('/', (req, res) => res.send('This is root.'))
+
+router.post('/sign-up', controllers.signUp)
+router.post('/sign-in', controllers.signIn)
+router.put('/change-password', restrict.controllers.changePassword)
+router.get('/users/:id', controllers.getUserById)
+router.get('/items', controllers.getAllItems)
+router.get('items/:id', controllers.getItemById)
+router.post('items/:id', restrict.controllers.createItem)
+router.put('items/:id', restrict.controllers.updateItem)
+router.delete('items/:id', restrict.controllers.deleteItem)
+
+module.exports = router
