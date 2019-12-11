eaa5c578a803f969efa8dcdfebf38964d3757657
Yev <yevyork@gmail.com>
2019-12-10 20:45:09 -0500

    add subheader and category component

---
+++ client/src/components/shared/SubHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Categories from './Categories'
+
+const SubHeader = () => (
+    <div className = 'subheader'>
+        <p>Buying at a flea market is easy,</p>
+        <p>Buying at E•FLEA is easier.</p>
+        <Categories />
+    </div>
+)
+
+export default SubHeader
